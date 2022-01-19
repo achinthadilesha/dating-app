cb3f22cf8ed7265f52d38f18d3bcb644bb370751
Achintha Dilesha <achintha.dilesha@gmail.com>
2022-01-19 13:35:31 +0530

    added angular project

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{
+{   
     //api controller
     [ApiController]
     [Route("api/[controller]")]
